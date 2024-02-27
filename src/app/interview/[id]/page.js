5dac75516ca5e1201d660ea4e66b4b275accaf4e
uzair <uzairchaudhary.30@gmail.com>
2024-02-27 16:35:40 +0500

    interview page setup

--- src/app/interview/[id]/page.js
+++ src/app/interview/[id]/page.js
@@ -1,7 +1,30 @@
-import React from 'react'
+"use client"
+import React, {useEffect,useState} from 'react'
 
-export default function page() {
+export default function page({params}) {
+    const [videoQuestionURLs, setvideoQuestionURLs] = useState();
+    
+    useEffect(() => {
+    
+        var requestOptions = {
+          method: 'GET',
+          redirect: 'follow'
+        };
+        
+        fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/job/getJobById/${params.id}`, requestOptions)
+          .then(response => response.json())
+          .then(result => {
+            if(result.success){
+                console.log(result)
+                setvideoQuestionURLs(result.videoURLs)
+            }
+        })
+          .catch(error => console.log('error', error));
+    }, [])
+       
   return (
-    <div>page</div>
+    <div>
+       k
+    </div>
   )
 }
